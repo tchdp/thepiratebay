2ff644d81fb9d1ef9511625dcdc5471c4bf5361d
Danny Perez <dperez@amplify.com>
2014-04-27 17:34:52 -0400

    Adding browse to view all new popular torrents

--- lib/thepiratebay/search.rb
+++ lib/thepiratebay/search.rb
@@ -3,39 +3,56 @@ require 'open-uri'
 require 'uri'
 
 module ThePirateBay
-  class Search
+  
+   class Search
     attr_reader :torrents
     alias_method :results, :torrents
 
     def initialize(query, page = 0, sort_by = 99, category = 0)
 
       query = URI.escape(query)
-      doc = Nokogiri::HTML(open('http://thepiratebay.org/search/' + query + '/' + page.to_s + '/' + sort_by.to_s + '/' + category.to_s + ''))
-      torrents = []
-
-      doc.css('#searchResult tr').each do |row|
-        title = row.search('.detLink').text
-        next if title == ''
-
-        seeders     = row.search('td')[2].text.to_i
-        leechers    = row.search('td')[3].text.to_i
-        magnet_link = row.search('td a')[3]['href']
-        category    = row.search('td a')[0].text
-        url         = row.search('.detLink').attribute('href').to_s
-        torrent_id  = url.split('/')[2]
-
-        torrent = {:title       => title,
-                   :seeders     => seeders,
-                   :leechers    => leechers,
-                   :magnet_link => magnet_link,
-                   :category    => category,
-                   :torrent_id  => torrent_id,
-                   :url         => url}
-
-        torrents.push(torrent)
-      end
-
-      @torrents = torrents
+      full_url = "http://thepiratebay.org/search/#{query}/#{page.to_s}/#{sort_by.to_s}/#{category.to_s}"
+      doc = Nokogiri::HTML(open(full_url))
+      
+      @torrents = ThePirateBay.get_torrents_from_doc doc
     end
   end
+
+  class Browse
+    attr_reader :torrents
+    alias_method :results, :torrents
+
+    def initialize(page = 0, sort_by = 99, category = 0)
+      full_url = "http://thepiratebay.org/browse/#{category.to_s}/#{page.to_s}/#{sort_by.to_s}"
+      doc = Nokogiri::HTML(open(full_url))
+      
+      @torrents = ThePirateBay.get_torrents_from_doc doc
+    end
+  end
+
+  def self.get_torrents_from_doc(html_doc)
+    torrents = []
+    html_doc.css('#searchResult tr').each do |row|
+      title = row.search('.detLink').text
+      next if title == ''
+
+      seeders     = row.search('td')[2].text.to_i
+      leechers    = row.search('td')[3].text.to_i
+      magnet_link = row.search('td a')[3]['href']
+      category    = row.search('td a')[0].text
+      url         = row.search('.detLink').attribute('href').to_s
+      torrent_id  = url.split('/')[2]
+
+      torrent = {:title       => title,
+       :seeders     => seeders,
+       :leechers    => leechers,
+       :magnet_link => magnet_link,
+       :category    => category,
+       :torrent_id  => torrent_id,
+       :url         => url}
+
+       torrents.push(torrent)
+     end
+     torrents
+   end
 end
