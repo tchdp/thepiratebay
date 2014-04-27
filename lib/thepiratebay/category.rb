03465942b0440a8d657cc2a173ea4bff43109824
Danny Perez <dperez@amplify.com>
2014-04-27 17:30:34 -0400

    Adding sub categories for Videos

--- lib/thepiratebay/category.rb
+++ lib/thepiratebay/category.rb
@@ -1,9 +1,15 @@
 module ThePirateBay
-  module Category
-    Audio          = "100"
-    Video          = "200"
-    Applications   = "300"
-    Games          = "400"
-    Other          = "600"
-  end
+	module Category
+		module Video
+			All          = "200"
+			Movies		   = "201"
+			HD_Movies		= "207"
+			HD_TV_Shows		= "208"
+		end
+		Audio          = "100"
+		
+		Applications   = "300"
+		Games          = "400"
+		Other          = "600"
+	end
 end
